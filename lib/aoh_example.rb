783ac27b67fe820447cca17216e8fe9478441339
Luke Lin <lukelin1991@yahoo.com>
2019-11-26 22:24:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,7 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
-  return literal_aoh
+  return new_array
 end
 
 def aoh_lookup(aoh, row, key)
